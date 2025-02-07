fcb723816df6bb8f42bd1979099a1de9de11e287
AleEsp76 <aem767676@gmail.com>
2025-02-07 05:55:52 -0600

    Primer Commit, Veterinaria Mundo Animal

---
+++ src/main/java/com/practica2_mundoanimal/Practica2MundoanimalApplication.java
@@ -0,0 +1,13 @@
+package com.practica2_mundoanimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2MundoanimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2MundoanimalApplication.class, args);
+	}
+
+}
